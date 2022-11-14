258cc32c7f00ea42c24035750b133485fe3bc8c3
lessamatheuss01 <matheus.lessa@sptech.school>
2022-11-14 14:22:26 -0300

    site estático 1.1

---
+++ site/src/database/script-tabelas.sql
@@ -0,0 +1,29 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql - banco local - ambiente de desenvolvimento
+*/
+
+create database valorantomega;
+
+use valorantomega;
+
+create table usuario(
+idusuario int primary key auto_increment,
+nome varchar(45),
+sobrenome varchar(45),
+email varchar(45), constraint chkemail check (email like '%@%'),
+senha varchar(45)
+);
+
+CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
+WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
+DEFAULT_SCHEMA = dbo;
+
+EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+
+EXEC sys.sp_addrolemember @rolename = N'db_datareader',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
